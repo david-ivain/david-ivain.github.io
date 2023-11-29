ce5ed88b9af65b872a1deb162bac04e3c2b25be3
David Ivain <david@MacBook-Pro-de-David.local>
2023-11-29 22:19:06 +0100

    init

---
+++ js/components/AppWindow.js
@@ -0,0 +1,109 @@
+export default class AppWindow extends HTMLElement {
+	position = {
+		x: 0,
+		y: 0,
+	};
+	offsetMouse = {
+		x: 0,
+		y: 0,
+	};
+	isMoving = false;
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.innerHTML = `
+			<div class="window__handle">
+				<div class="window__controls">
+					<button
+						type="button"
+						class="window__control"
+						data-variant="close">
+						<span>close</span>
+					</button>
+					<button
+						type="button"
+						class="window__control"
+						data-variant="minimize">
+						<span>Minimize</span>
+					</button>
+					<button
+						type="button"
+						class="window__control"
+						data-variant="maximize">
+						<span>Maximize</span>
+					</button>
+				</div>
+				<div class="window__title">${this.getAttribute("window-title")}</div>
+			</div>
+			<div class="window__content">
+				${this.innerHTML}
+			</div>
+		`;
+
+		const $maybeCloseButton = this.querySelector(
+			'.window__control[data-variant="close"]'
+		);
+		const $maybeMinimizeButton = this.querySelector(
+			'.window__control[data-variant="minimize"]'
+		);
+		const $maybeMaximizeButton = this.querySelector(
+			'.window__control[data-variant="maximize"]'
+		);
+		const $maybeHandle = this.querySelector(".window__title");
+
+		$maybeCloseButton?.addEventListener("click", () => this.remove());
+		$maybeMaximizeButton?.addEventListener("click", () => {
+			this.classList.toggle("maximized");
+		});
+		$maybeMinimizeButton?.addEventListener("click", () => {
+			this.classList.toggle("minimized");
+		});
+		$maybeHandle?.addEventListener("mousedown", this._handleStartMoving);
+		$maybeHandle?.addEventListener("dblclick", () => {
+			this.classList.toggle("maximized");
+		});
+	}
+
+	/** @type {(event: MouseEvent) => void} */
+	_handleStartMoving = (event) => {
+		if (this.classList.contains("maximized")) return;
+		this.position = {
+			x: this.getBoundingClientRect().left,
+			y: this.getBoundingClientRect().top,
+		};
+		this.offsetMouse = {
+			x: event.clientX - this.position.x,
+			y: event.clientY - this.position.y,
+		};
+		this.isMoving = true;
+		this.classList.add("moving");
+		document.addEventListener("mousemove", this._handleDuringMoving);
+		document.addEventListener("mouseup", this._handleEndMoving, {
+			once: true,
+		});
+		this._moveWindow();
+	};
+
+	_handleEndMoving = () => {
+		this.classList.remove("moving");
+		this.isMoving = false;
+		document.removeEventListener("mousemove", this._handleDuringMoving);
+	};
+
+	/** @type {(event: MouseEvent) => void} */
+	_handleDuringMoving = (event) => {
+		this.position = {
+			x: event.clientX - this.offsetMouse.x,
+			y: event.clientY - this.offsetMouse.y,
+		};
+	};
+
+	_moveWindow = () => {
+		this.style.left = this.position.x + "px";
+		this.style.top = this.position.y + "px";
+		if (this.isMoving) requestAnimationFrame(this._moveWindow);
+	};
+}
---
+++ js/main.js
@@ -0,0 +1,47 @@
+import AppIcon from "/js/components/AppIcon.js";
+import AppWindow from "/js/components/AppWindow.js";
+
+customElements.define("app-icon", AppIcon);
+customElements.define("app-window", AppWindow);
+
+document.querySelector("#login-form")?.addEventListener("submit", (event) => {
+	console.log("[EVENTS] login form submitted");
+	event.preventDefault();
+	const $login = document.querySelector(".login");
+	$login?.addEventListener(
+		"animationend",
+		(event) => {
+			$login?.classList.add("deleted");
+			$login?.classList.remove("deleting");
+		},
+		{ once: true }
+	);
+	$login?.classList.add("deleting");
+	document.querySelector(".desktop")?.classList.remove("deleted");
+});
+
+/**
+ * The following is done not to bother the user with unwanted password manager
+ * prompts.
+ * It's a mock website so it's unnecessary to have a proper password field.
+ */
+
+let passwordTimeout = 0;
+document
+	.querySelector("#login-password")
+	?.addEventListener("keypress", (event) => {
+		const $field = event.currentTarget;
+		$field.value = $field?.value.replace(/./g, "•");
+		if (passwordTimeout) clearTimeout(passwordTimeout);
+		passwordTimeout = setTimeout(() => {
+			$field.value = $field?.value.replace(/./g, "•");
+		}, 500);
+	});
+
+/**
+ * Development code. to toggle off certain parts.
+ */
+
+const SKIP_LOGIN = true;
+if (SKIP_LOGIN)
+	document.querySelector("#login-form")?.dispatchEvent(new Event("submit"));
