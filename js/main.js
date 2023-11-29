fa03203b7cb7e2d6b9f7a993d098275a7d686c55
David Ivain <david@MacBook-Pro-de-David.local>
2023-11-29 22:23:26 +0100

    config: enable login

--- js/main.js
+++ js/main.js
@@ -42,6 +42,6 @@ document
  * Development code. to toggle off certain parts.
  */
 
-const SKIP_LOGIN = true;
+const SKIP_LOGIN = false;
 if (SKIP_LOGIN)
 	document.querySelector("#login-form")?.dispatchEvent(new Event("submit"));
